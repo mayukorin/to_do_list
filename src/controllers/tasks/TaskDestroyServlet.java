a7d4423a8c64b881816774075b50e59869a2867c
mayukorin <19t7b2fx@gmail.com>
2020-09-21 10:30:38 +0900

    Modify TaskDestroy

--- src/controllers/tasks/TaskDestroyServlet.java
+++ src/controllers/tasks/TaskDestroyServlet.java
@@ -1,6 +1,7 @@
 package controllers.tasks;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Person;
+import models.Show;
 import models.Task;
 import utils.DBUtil;
 
@@ -38,17 +41,79 @@ public class TaskDestroyServlet extends HttpServlet {
 
             Task t = em.find(Task.class, (Integer)request.getSession().getAttribute("task_id"));
 
-            em.getTransaction().begin();
-            em.remove(t);       // データ削除
-            em.getTransaction().commit();
-            em.close();
+           //そのtaskを公開しているshowを取り出す
+            List<Show> shows = em.createNamedQuery("getShows",Show.class).setParameter("task", t).getResultList();
 
+            if (t.getAccount() instanceof Person) {
+                //自分自身のtaskを削除しようとしている時
+
+
+
+                if (shows != null) {
+                    //そのtaskを公開しているshowがあれば
+                    //そのshowを一つずつ削除する
+
+                    for (Show show:shows) {
+                        em.getTransaction().begin();
+                        em.remove(show);
+                        em.getTransaction().commit();
+                    }
+                }
+
+                //taskを削除する
+                em.getTransaction().begin();
+                em.remove(t);
+                em.getTransaction().commit();
+            } else {
+                //groupのtaskを削除しようとしている時
+                //①showを削除する
+                if (shows != null) {
+                    //そのtaskを公開しているshowがあれば
+                    //そのshowを一つずつ削除する
+
+                    for (Show show:shows) {
+                        em.getTransaction().begin();
+                        em.remove(show);
+                        em.getTransaction().commit();
+                    }
+                }
+
+                //②過去のtask履歴を全て削除する/////////////////////////////////////////////////////////////
+                if (t.getOrigin_task_id() != null) {
+                    //Groupのtaskかつ過去に更新してきているtaskの時
+                    //過去のtaskを全てとってくる
+                    List<Task> tasks_history = em.createNamedQuery("GetTaskHistroy",Task.class).setParameter("origin_task_id",t.getOrigin_task_id()).getResultList();
+
+                    //task履歴を全て削除
+                    for (Task task:tasks_history) {
+                        em.getTransaction().begin();
+                        em.remove(task);
+                        em.getTransaction().commit();
+                    }
+                } else {
+                    //過去のtask履歴は存在しない時
+                    em.getTransaction().begin();
+                    em.remove(t);
+                    em.getTransaction().commit();
+                }
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            }
+
+            em.close();
             request.getSession().removeAttribute("task_id");
 
-         // indexページへリダイレクト
-            response.sendRedirect(request.getContextPath() + "/toppage/index");
+            request.getSession().setAttribute("flush", "taskの削除が完了しました。");
 
+            if (request.getSession().getAttribute("group") == null) {
+                //グループ画面からtaskを追加していない時
+                response.sendRedirect(request.getContextPath()+"/toppage/index");//ホーム画面に戻る
+            } else {
+                //グループ画面からtaskを追加した時
+                response.sendRedirect(request.getContextPath()+"/groups/member");//taskを追加したメンバーのtask一覧画面に戻る
+
+            }
         }
     }
-
 }
--- src/models/Show.java
+++ src/models/Show.java
@@ -20,6 +20,9 @@ import javax.persistence.Table;
         @NamedQuery(
                 name = "getGroupShow",
                 query = "select s.group from Show as s where s.task = :task"),
+        @NamedQuery(
+                name = "getShows",
+                query = "select s from Show as s where s.task = :task")
 
 })
 @Entity
